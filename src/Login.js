83a1ab91e8f564ffc582e623a3e1e84238b8c0d8
rng190001 <rng190001@utdallas.edu>
2022-06-11 17:35:53 -0500

    added new implementations

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className = "login">
+      <h1>Spotify Spaces</h1>
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
